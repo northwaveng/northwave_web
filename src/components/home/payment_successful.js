6d2dafa7ac7360d3717e3746b40c5ce24225311b
ibukunoluwanap <promisenap2001@gmail.com>
2023-08-08 16:25:51 +0300

    added payment successful page

--- src/components/home/payment_successful.js
+++ src/components/home/payment_successful.js
@@ -4,9 +4,13 @@ export default function PaymentSuccessful() {
         <>
             <div className="container py-5">
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-12 text-center">
+                        <img src="/logo/logo.png" width={200} />
+                    </div>
+
+                    <div className="col-12 text-center mt-5">
                         <h1 className="text-black my-2">Payment Successful</h1>
-                        <p>You can now click on the back button</p>
+                        <p>You can now click on the back button now!</p>
                     </div>
                 </div>
             </div>
