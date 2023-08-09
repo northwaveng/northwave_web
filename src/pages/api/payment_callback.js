19f303a8d1dbda3728813675a8467ffa5aec82e3
ibukunoluwanap <promisenap2001@gmail.com>
2023-08-09 20:02:10 +0300

    adding collection function

--- src/pages/api/payment_callback.js
+++ src/pages/api/payment_callback.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { doc, updateDoc, addDoc, collection, getDoc } from 'firebase/firestore';
 import { db } from '../../firebase/fire_config';
-import { auth } from "@/firebase/fire_config";
 import Cryptograph from '@/components/utils/cryptograph';
 
 export default async function handler(req, res) {
@@ -44,18 +43,20 @@ export default async function handler(req, res) {
               const members = snapshot.data().members;
               const collector = members[0];
 
-              getDoc(doc(db, 'users', collector)).then((user) => {
-                const data = user.data();
-                const crypto = new Cryptograph();
-                const accountNumber = crypto.decrypt({ value: data.kyc.accountNumber });
+              res.status(200).json({ status: 'success', message: collector });
 
-                res.status(200).json({ status: 'success', message: accountNumber });
+              // getDoc(doc(db, 'users', collector)).then((user) => {
+              //   const data = user.data();
+              //   const crypto = new Cryptograph();
+              //   const accountNumber = crypto.decrypt({ value: data.kyc.accountNumber });
 
-                // res.setHeader('Location', `${process.env.NEXT_PUBLIC_DOMAIN}payment_successful`);
-                // res.status(302).end();
-              }).catch((error) => {
-                res.status(200).json({ status: 'error', message: `Something is wrong: ${error.message}` });
-              });
+              //   res.status(200).json({ status: 'success', message: accountNumber });
+
+              //   // res.setHeader('Location', `${process.env.NEXT_PUBLIC_DOMAIN}payment_successful`);
+              //   // res.status(302).end();
+              // }).catch((error) => {
+              //   res.status(200).json({ status: 'error', message: `Something is wrong: ${error.message}` });
+              // });
             }
 
           }).catch((error) => {
