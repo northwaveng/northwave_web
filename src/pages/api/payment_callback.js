6d970e00cc1588683bc7de4de64a3eb5f9521c1c
ibukunoluwanap <promisenap2001@gmail.com>
2023-08-09 20:18:59 +0300

    adding collection function

--- src/pages/api/payment_callback.js
+++ src/pages/api/payment_callback.js
@@ -41,28 +41,29 @@ export default async function handler(req, res) {
             const target = parseFloat(snapshot.data().target);
             const contributions = parseFloat(totalContributions);
 
-            if (contributions > target) {
-              const members = snapshot.data().members;
-              const collector = members[0];
+            res.status(200).json({ status: 'success', contributions: contributions, target: target, passed: (contributions > target) });
+            // if (contributions > target) {
+            // const members = snapshot.data().members;
+            // const collector = members[0];
 
-              res.status(200).json({ status: 'success', message: collector });
+            // res.status(200).json({ status: 'success', message: collector });
 
-              //     getDoc(doc(db, 'users', collector)).then((user) => {
-              //       const data = user.data();
-              //       const crypto = new Cryptograph();
-              //       const accountNumber = crypto.decrypt({ value: data.kyc.accountNumber });
+            //     getDoc(doc(db, 'users', collector)).then((user) => {
+            //       const data = user.data();
+            //       const crypto = new Cryptograph();
+            //       const accountNumber = crypto.decrypt({ value: data.kyc.accountNumber });
 
-              //       res.status(200).json({ status: 'success', message: accountNumber });
+            //       res.status(200).json({ status: 'success', message: accountNumber });
 
-              //       // res.setHeader('Location', `${process.env.NEXT_PUBLIC_DOMAIN}payment_successful`);
-              //       // res.status(302).end();
-              //     }).catch((error) => {
-              //       res.status(200).json({ status: 'error', message: `Something is wrong: ${error.message}` });
-              //     });
-            } else {
-              res.setHeader('Location', `${process.env.NEXT_PUBLIC_DOMAIN}payment_successful`);
-              res.status(302).end();
-            }
+            //       // res.setHeader('Location', `${process.env.NEXT_PUBLIC_DOMAIN}payment_successful`);
+            //       // res.status(302).end();
+            //     }).catch((error) => {
+            //       res.status(200).json({ status: 'error', message: `Something is wrong: ${error.message}` });
+            //     });
+            // } else {
+            //   res.setHeader('Location', `${process.env.NEXT_PUBLIC_DOMAIN}payment_successful`);
+            //   res.status(302).end();
+            // }
           }).catch((error) => {
             res.status(200).json({ status: 'error', message: `Something is wrong: ${error.message}` });
           });
