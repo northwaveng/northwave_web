f984de00e91ca9ebdf2e0316471a9cb744e545e7
ibukunoluwanap <promisenap2001@gmail.com>
2023-08-11 20:23:12 +0300

    fix bugs and updated web app

--- src/components/dashboard/verification.js
+++ src/components/dashboard/verification.js
@@ -1,22 +1,25 @@
 import styles from '@/components/auth/Auth.module.css'
 import Link from 'next/link';
 import { db } from "@/firebase/fire_config";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { toast } from "react-toastify";
 import Loader from '@/components/loader/loader';
-import { doc, updateDoc } from 'firebase/firestore';
+import { doc, updateDoc, getDoc } from 'firebase/firestore';
 import { useRouter } from 'next/router';
-import Cryptograph from '@/components/utils/cryptograph';
 import { useAuth } from '@/firebase/fire_auth_context';
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import randomNum from '@/components/utils/randomNum'
+import axios from 'axios';
 
 const storage = getStorage();
 
 export default function Signin() {
     const [loading, setLoading] = useState(false);
+    const [banks, setBanks] = useState([]);
     const [id, setId] = useState(null);
     const [accountNumber, setAccountNumber] = useState("");
+    const [bankName, setBankName] = useState("");
+    const [bankCode, setBankCode] = useState("");
     const [workAddress, setWorkAddress] = useState("");
     const [monthlyIncome, setMonthlyIncome] = useState("");
     const [bvn, setBvn] = useState("");
@@ -24,40 +27,128 @@ export default function Signin() {
     const { authUser } = useAuth();
     const router = useRouter();
 
+    useEffect(() => {
+        const fetchBankList = async () => {
+            try {
+                let banks_ = [];
+
+                const url = `${process.env.NEXT_PUBLIC_PAYSTACK_HOSTNAME}bank?currency=NGN&country=nigeria`;
+                const headers = { Authorization: `Bearer ${process.env.NEXT_PUBLIC_PAYSTACK_TEST_SECRET_KEY}` };
+
+                const response = await axios.get(url, { headers });
+
+                if (response.status === 200 || response.status === 201) {
+                    const data = response.data;
+                    const bankListData = data["data"];
+
+                    for (const bank of bankListData) {
+                        const bankModel = { name: bank["name"], code: bank["code"] };
+                        banks_.push(bankModel);
+                    }
+
+                    setBanks(banks_);
+                }
+            } catch (error) {
+                toast.error(`Something went wrong: ${error}`);
+            }
+        };
+
+        fetchBankList();
+    }, []);
+
     const onVerification = async event => {
         event.preventDefault();
         setLoading(true);
-        toast.info("Uploading ID.");
 
-        const idRef = ref(storage, `idCards/idCard${randomNum(6)}`);
-        await uploadBytes(idRef, id);
+        if (bankCode.length <= 0) {
+            toast.error("Please select bank.");
+            setLoading(false);
+        } else {
+            toast.info("Validating account number.");
+            const path = `bank/resolve?account_number=${accountNumber}&bank_code=${bankCode}`;
+            const url = `${process.env.NEXT_PUBLIC_PAYSTACK_HOSTNAME}${path}`;
+            const headers = { Authorization: `Bearer ${process.env.NEXT_PUBLIC_PAYSTACK_TEST_SECRET_KEY}` };
 
-        await getDownloadURL(idRef).then(async (idImage) => {
-            toast.info("Verifying...")
+            try {
+                const response = await axios.get(url, { headers });
 
-            // user doc
-            const userDoc = doc(db, "users", authUser.email);
+                if (response.status === 200 || response.status === 201) {
+                    const userDoc = doc(db, "users", authUser.email);
+                    await getDoc(userDoc).then(async (snap) => {
+                        toast.info("Uploading ID.");
 
-            // Data to update
-            const data = {
-                "kyc.id": idImage,
-                "kyc.accountNumber": accountNumber,
-                "kyc.workAddress": workAddress.toLowerCase(),
-                "kyc.monthlyIncome": monthlyIncome,
-                "kyc.bvn": bvn,
-                "kyc.nin": nin,
-            };
+                        const idRef = ref(storage, `idCards/idCard${randomNum(6)}`);
+                        await uploadBytes(idRef, id);
 
-            // Update the data
-            await updateDoc(userDoc, data).then(() => {
-                toast.success("You are Verified.");
-                router.push("/dashboard");
-            }).catch((error) => {
-                toast.error(`Something is wrong: ${error.message}`);
-            });
-        }).catch((error) => {
-            toast.error(`Something is wrong: ${error.message}`);
-        });
+                        await getDownloadURL(idRef).then(async (id) => {
+                            toast.info("Verifying user.")
+
+                            const name = `${snap.data().firstName} ${snap.data().lastName}`;
+                            const url_ = `${process.env.NEXT_PUBLIC_PAYSTACK_HOSTNAME}transferrecipient`;
+                            const headers_ = { Authorization: `Bearer ${process.env.NEXT_PUBLIC_PAYSTACK_TEST_SECRET_KEY}`, 'Content-Type': 'application/json' };
+                            const params_ = JSON.stringify({
+                                "type": "nuban",
+                                "name": name,
+                                "account_number": accountNumber,
+                                "bank_code": bankCode,
+                                "currency": "NGN"
+                            });
+
+                            try {
+                                const response_ = await axios.post(url_, params_, { headers: headers_ });
+
+                                if (response_.status === 200 || response_.status === 201) {
+                                    let resData = response_.data;
+                                    const recipientCode = resData["data"]["recipient_code"];
+
+                                    // user doc
+                                    const userDoc = doc(db, "users", authUser.email);
+
+                                    // Data to update
+                                    const data = {
+                                        "kyc.id": id,
+                                        "kyc.accountNumber": accountNumber,
+                                        "kyc.bankCode": bankCode,
+                                        "kyc.recipientCode": recipientCode,
+                                        "kyc.workAddress": workAddress.toLowerCase(),
+                                        "kyc.monthlyIncome": monthlyIncome,
+                                        "kyc.bvn": bvn,
+                                        "kyc.nin": nin,
+                                    };
+
+                                    // Update the data
+                                    await updateDoc(userDoc, data).then(() => {
+                                        toast.success("You are Verified.");
+                                        router.push("/dashboard");
+                                    }).catch((error) => {
+                                        toast.error(`Something is wrong: ${error.message}`);
+                                        setLoading(false);
+                                    });
+                                }
+                            } catch (error) {
+                                toast.error(error);
+                                setLoading(false);
+                            }
+                        }).catch((error) => {
+                            toast.error(`Something is wrong: ${error.message}`);
+                            setLoading(false);
+                        });
+                    }).catch((error) => {
+                        toast.error(`Something is wrong: ${error.message}`);
+                        setLoading(false);
+                    });
+                }
+            } catch (error) {
+                toast.error("Couldn't validate account number.");
+                setLoading(false);
+            }
+        }
+    };
+
+    const onSelectBank = (selectedBank) => {
+        setBankName(selectedBank.name);
+        setBankCode(selectedBank.code);
+        toast.success(`${selectedBank.name} selected.`)
     };
 
     return (
@@ -94,6 +185,20 @@ export default function Signin() {
                                     />
                                     <label htmlFor="id">ID Card</label>
                                 </div>
+
+                                {banks.length <= 0 ?
+                                    <div className="mx-2 mb-3">
+                                        <p>Loading bank list</p>
+                                        <Loader />
+                                    </div>
+                                    :
+                                    <div className="mx-2 mb-3">
+                                        <button type="button" className="form-control p-3" data-bs-toggle="modal" data-bs-target="#backList">
+                                            {bankCode.length > 0 ? bankName : "Click and select bank"}
+                                        </button>
+                                    </div>
+                                }
+
 
                                 <div className="d-flex mb-3">
                                     <div className="col-6">
@@ -183,6 +288,28 @@ export default function Signin() {
                     <footer className="mt-auto text-muted">
                         For further support, you may visit the <Link href="/contact" className="text-decoration-none primary">contact.</Link>
                     </footer>
+
+                    <div className="modal fade" id="backList" tabIndex="-1" aria-labelledby="backListLabel" aria-hidden="true">
+                        <div className="modal-dialog modal-dialog-scrollable">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">Banks In Nigeria</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <ul className="list-group">
+                                        {banks.map((bank) => (
+                                            <li key={bank.code} className="list-group-item text-start" style={{ cursor: "pointer" }} onClick={() => onSelectBank(bank)}>
+                                                {bank.name}
+                                                <br />
+                                                <small className="text-muted">{bank.code}</small>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
